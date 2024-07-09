4cc52f9843161225c4f4d47b744531e07ea89bbb
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-07-09 13:46:02 +0530

    Update main.go

--- booking_app/main.go
+++ booking_app/main.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func main(){
-	var eventname= "Frosh"
+	eventname := "digital gandhi"
 	const conferenceTickets=350
-	var remainingtickets=350
+	var remainingtickets uint=350
 
 	fmt.Printf("welcome to %v powered by frosh\n  ", eventname)
 	fmt.Println("we have a total of", conferenceTickets, "tickets and", remainingtickets,"are still available")
 	fmt.Println("get your tickets right now ")
+	fmt.Scan()
+
+	var username ="tom"
+	fmt.Println((username))
 }
 
