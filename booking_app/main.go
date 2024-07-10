e4fc0e743e48146101e008e9450e3506a0adca31
Riteshk1314 <riteshkapoor1314@gmail.com>
2024-07-10 09:48:00 +0530

    learning update 10/7/24 morning

--- booking_app/main.go
+++ booking_app/main.go
@@ -7,12 +7,35 @@ func main(){
 	const conferenceTickets=350
 	var remainingtickets uint=350
 
-	fmt.Printf("welcome to %v powered by frosh\n  ", eventname)
+	fmt.Printf("welcome to %v powered by \n  ", eventname)
 	fmt.Println("we have a total of", conferenceTickets, "tickets and", remainingtickets,"are still available")
 	fmt.Println("get your tickets right now ")
-	fmt.Scan()
 
-	var username ="tom"
-	fmt.Println((username))
+	var bookings =[]string{"nicole", "peter"}
+	
+	var firstname string 
+	var lastname string
+	var email string 
+	var userticket int 
+
+
+	for {
+	fmt.Println(("enter your first name"))
+	fmt.Scan(&firstname)
+	fmt.Println(&firstname)
+	fmt.Println(("enter your last name"))
+	fmt.Scan(&lastname)
+	fmt.Println(("enter your email "))
+	fmt.Scan(&email)
+	fmt.Println(("enter number of tickets "))
+	fmt.Scan(&userticket)
+
+	remainingtickets= remainingtickets - uint(userticket)
+	bookings=append(bookings, firstname +" "+lastname)
+	println("array length", len(bookings))
+	fmt.Printf("thank you %v %v for booking %v tickets for %v event, you will receive a confirmation email at %v ",firstname, lastname, userticket, eventname, email)	
+	fmt.Printf("remaining tickets %v", remainingtickets)
+
+	}
 }
 
